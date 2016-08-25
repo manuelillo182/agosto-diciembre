98bec45b74f31acf2a6ff8a9115e399f35cca955
teto_ <teto_@DESKTOP-6QN7B48>
2016-08-25 11:29:32 -0600

    mejora

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,12 +13,16 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        static Checador checador = new Checador();
         static void Main(string[] args)
         {
             bool band = true;
-            TcpListener tcpListener = new TcpListener(System.Net.IPAddress.Any, 9898);
-            Checador checador;
-           
+            
+          TcpListener tcpListener = new TcpListener(System.Net.IPAddress.Any,9898);
+
+            string theString = "Se esta ejecutando el proceso..favor de esperar";
+
+            INICIO:
             while (band)
             {
                 //Iniciamos la esucha
@@ -29,98 +33,128 @@ namespace ConsoleApplication1
                 //Console.WriteLine("Esto es una prueba");
                 if (socketForClient.Connected)
                 {
-                  
-
-                    Console.WriteLine("se conecto");
                         NetworkStream networkStream = new NetworkStream(socketForClient);
-                        StreamWriter streamWriter = new StreamWriter(networkStream);
+                         StreamWriter streamWriter = new StreamWriter(networkStream);
                         StreamReader streamReader = new StreamReader(networkStream);
-
-                        string theString = "Se esta ejecutando el proceso..favor de esperar";
+                        streamWriter.AutoFlush = true;
+                    try
+                        {
+                        //Escribimos la data en el stream
                         try
                         {
-                      
-                            //Escribimos la data en el stream
-                            streamWriter.WriteLine(theString);
-                            //Ahora le decimos que la mande.
-                            streamWriter.Flush();
-
-                        Console.WriteLine("intenta recibir mensaje");
-                        theString = streamReader.ReadLine();
-                        Console.WriteLine(theString);
-                        Console.WriteLine("recibio el mensaje");
-
-                        //empieza el proceso con el checador
-                        checador = new Checador();
-                        checador.Aparato = 1;
-                        string[] direcc = { "10.12.4.10", "10.7.6.199", "10.12.102.20", "10.4.4.10", "10.7.4.10", "10.2.6.10", "10.4.6.6","10.4.7.27","10.2.6.11","10.6.6.253",
-                                 "10.14.4.10", "10.12.16.15", "10.12.4.11"};
-                        checador.Puerto = 4370;
-
-                        for (int i = 0; i <= direcc.Length - 1; i++)
-
-                        {
-                            checador.cDireccion = direcc[i];
-                            //realiza ping para obtener estado de dispositivo
-                            Ping HacerPing = new Ping();
-                            PingReply RespuestaPing;
-                            RespuestaPing = HacerPing.Send(checador.cDireccion);
-                            //
-                            if (RespuestaPing.Status == IPStatus.Success)
-                            {
-                                try
-                                {
-                                    checador.Conectar();
-                                    Console.WriteLine("\n" + "ip:" + checador.cDireccion + "-puerto:" + checador.Puerto);
-                                    if (checador.Conectado)
-                                    {
-                                        checador.GenerarListaDeChecador();
-                                        checador.muestra();
-                                    }
-                                    else
-                                    {
-                                        MessageBox.Show("Error al conectar al checador, error=44", "Error");
-                                    }
-                                }
-
-#pragma warning disable CS0168 // La variable está declarada pero nunca se usa
-                                catch (Exception ex)
-#pragma warning restore CS0168 // La variable está declarada pero nunca se usa
-                                {
-                                    MessageBox.Show("Error al conectar al checador, error=" + checador.Error().ToString(), "Error");
-                                }
-                            }
-                            else
-                            {
-                                Console.WriteLine("\n El dispositivo " + checador.cDireccion + " No esta conectado");
-
-                                //Escribimos la data en el stream
-                                streamWriter.WriteLine("El dispositivo " + checador.cDireccion + " No esta conectado");
-                                //Ahora le decimos que la mande.
-                                streamWriter.Flush();
-                            }
-                        }
-                        MessageBox.Show("proceso terminado");
-
-                        //Escribimos la data en el stream
-                        streamWriter.WriteLine("e");
-                        //Ahora le decimos que la mande.
-                        streamWriter.Flush();
-
-                        theString = streamReader.ReadLine();
+                            theString = streamReader.ReadLine() ;
                             Console.WriteLine(theString);
+                        }catch (Exception e)
+                        {
+                            Console.WriteLine("error de comunicacion con cliente");
+                            goto INICIO;
                         }
+
+
+                        Char delimiter = ',';
+                        String[] substrings = theString.Split(delimiter);
+                        foreach (var substring in substrings)
+                            Console.WriteLine(substring);
+
+
+                        try
+                        {
+                            streamWriter.WriteLine(theString);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("error de comunicacion con cliente");
+                            goto INICIO;
+                        }
+
+                        //Console.WriteLine("entra try");
+                        foreach (var substring in substrings)                            
+                        {
+                            Console.WriteLine("Envia"+substring);
+                            //Console.WriteLine(x);
+                            try
+                            {
+                                streamWriter.WriteLine("analizando:"+substring);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("error de comunicacion con cliente");
+                                goto INICIO;
+                            }
+                            Thread.Sleep(1000);
+                        }
+                        try
+                        {
+                            streamWriter.WriteLine("s");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("error de comunicacion con cliente");
+                            goto INICIO;
+                        }
+                    }
                         finally
                         {
                             streamReader.Close();
                             streamWriter.Close();
                             networkStream.Close();
                             socketForClient.Close();
+                        Console.WriteLine("Cierra Reader,Writer,networkStream, y socket");
                         }
                     
                    
                     tcpListener.Stop();
+                    Console.WriteLine("detiene tcplistener");
 
+                }
+            }
+        }
+
+        public void checa()
+        {
+           
+            checador.Aparato = 1;
+            //string[] direcc = { "10.12.4.10", "10.7.6.199", "10.12.102.20", "10.4.4.10", "10.7.4.10", "10.2.6.10", "10.4.6.6","10.4.7.27","10.2.6.11","10.6.6.253",
+            //         "10.14.4.10", "10.12.16.15", "10.12.4.11"};
+            string[] direcc = { "10.12.4.10", "10.7.6.199" };
+            checador.Puerto = 4370;
+
+            for (int i = 0; i <= direcc.Length - 1; i++)
+
+            {
+                checador.cDireccion = direcc[i];
+                //realiza ping para obtener estado de dispositivo
+                Ping HacerPing = new Ping();
+                PingReply RespuestaPing;
+                RespuestaPing = HacerPing.Send(checador.cDireccion);
+                //
+                if (RespuestaPing.Status == IPStatus.Success)
+                {
+                    try
+                    {
+                        checador.Conectar();
+                        Console.WriteLine("\n" + "ip:" + checador.cDireccion + "-puerto:" + checador.Puerto);
+                        if (checador.Conectado)
+                        {
+                            checador.GenerarListaDeChecador();
+                            checador.muestra();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error al conectar al checador, error=44", "Error");
+                        }
+                    }
+
+#pragma warning disable CS0168 // La variable está declarada pero nunca se usa
+                    catch (Exception ex)
+#pragma warning restore CS0168 // La variable está declarada pero nunca se usa
+                    {
+                        MessageBox.Show("Error al conectar al checador, error=" + checador.Error().ToString(), "Error");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\n El dispositivo " + checador.cDireccion + " No esta conectado");
                 }
             }
         }
